4f55a3b938be6061cdb0c72eed2de05c9035dbed
YamaokaShinsaku <20010017ys@edu.fca.ac.jp>
2022-10-02 21:43:01 +0900

    攻撃オブジェクトの修正
    武器を配列に格納して、順番に飛ばせるようにしたもの（J　で発射、　K　で装填）
    発射したオブジェクトは５秒後に消える

---
+++ Assets/Scripts/WeaponMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponMove : MonoBehaviour
+{
+    public float nowPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nowPosition = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position =
+            new Vector3(transform.position.x, nowPosition + Mathf.PingPong(Time.time / 3, 0.3f), transform.position.z);
+    }
+}
