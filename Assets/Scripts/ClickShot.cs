e81ed52b0f68468e0ef7217efc24512d8c7e4344
YamaokaShinsaku <20010017ys@edu.fca.ac.jp>
2022-10-04 16:22:23 +0900

    武器の追従処理の調整　Jointを使用

--- Assets/Scripts/ClickShot.cs
+++ Assets/Scripts/ClickShot.cs
@@ -7,13 +7,15 @@ public class ClickShot : MonoBehaviour
     public GameObject[] shotObj;    // 飛ばす武器
     public int count;               // 発射カウント
     private GameObject clone;       // 武器のクローン
-    private float nowPos;
+    public Vector3[] position;
+    public Quaternion[] rotation;
 
     public Vector3[] offset;
     public Transform target;
 
     public GameObject lookTarget;
     private Vector3 prevPosition;
+    FixedJoint component;
 
     // Start is called before the first frame update
     void Start()
@@ -24,59 +26,65 @@ public class ClickShot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 diff = shotObj[0].transform.position - prevPosition;
-        prevPosition = shotObj[0].transform.position;
+        //Vector3 diff = shotObj[0].transform.position - prevPosition;
+        ////prevPosition = shotObj[0].transform.position;
+        ////if (diff.magnitude > 0.01f)
+        ////{
+        ////    //shotObj[0].transform.rotation = Quaternion.LookRotation(diff);
+        ////    shotObj[0].transform.eulerAngles = new Vector3(90, Quaternion.LookRotation(diff).y, 0);
+        ////}
 
-        if (diff.magnitude > 0.01f)
+        //Quaternion rot = Quaternion.AngleAxis(-lookTarget.transform.rotation.y, Vector3.forward);
+        //Quaternion q = shotObj[0].transform.rotation;
+        //shotObj[0].transform.rotation = Quaternion.RotateTowards(shotObj[0].transform.rotation, q * rot, 5);
+
+        // 座標更新
+        for (int i = 0; i < shotObj.Length; i++)
         {
-            shotObj[0].transform.rotation = Quaternion.LookRotation(diff);
+            position[i] = shotObj[i].transform.position;
+            rotation[i] = shotObj[i].transform.rotation;
         }
 
         // 武器が残っているとき
         if (count < shotObj.Length)
         {
-
+            if (component == null)
+            {
+                component = shotObj[0].AddComponent<FixedJoint>();
+                component.connectedBody = lookTarget.GetComponent<Rigidbody>();
+            }
             // 武器がプレイヤーを追従するようにする
             for (int i = 0; i < shotObj.Length; i++)
             {
                 // 位置の計算
-                Vector3 position = shotObj[i].transform.position;
-
-                position.y = offset[i].y + Mathf.PingPong(Time.time / 3, 0.3f);
-                position.z = shotObj[i].transform.position.z;
-
-                shotObj[i].transform.position = target.transform.position + offset[i];
-
-                // 向きの計算
-                //Vector3 diff = shotObj[0].transform.position - prevPosition;
-                //prevPosition = shotObj[0].transform.position;
-
-                //if(diff.magnitude > 0.01f)
-                //{
-                //    transform.rotation = Quaternion.LookRotation(diff);
-                //}
-
-                //Vector3 direction = lookTarget.transform.position - shotObj[i].transform.position;
-                //direction.y = 0;
-                //Quaternion lookRotation = Quaternion.LookRotation(direction, Vector3.up);
-                //lookRotation.x = 1.0f;
+                //shotObj[i].transform.position = target.transform.position + offset[i];
                 
-                //shotObj[i].transform.rotation = Quaternion.Lerp(shotObj[i].transform.rotation, lookRotation, 0.1f);
-
-                //Debug.Log(shotObj[i].transform.rotation);
+                // 向きの計算
             }
 
             if (Input.GetKeyDown(KeyCode.J))
             {
-                clone = Instantiate(shotObj[count]);    // 武器のクローンを生成
-                shotObj[count].SetActive(false);        // 武器本体を非表示に
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                Vector3 dir = ray.direction;            // 飛ばす向き
-                clone.GetComponent<Rigidbody>().AddForce(dir * 3000);   // マウスの位置に飛ばす
+                Destroy(component);
+                if (component == null)
+                {
+                    clone = Instantiate(shotObj[count], position[count], rotation[count]);    // 武器のクローンを生成
+                    shotObj[count].SetActive(false);        // 武器本体を非表示に
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    Vector3 dir = ray.direction;            // 飛ばす向き
+                    clone.GetComponent<Rigidbody>().AddForce(dir * 3000);   // マウスの位置に飛ばす
+                    count++;
 
-                count++;
+                    // 5秒後にクローンを削除
+                    Destroy(clone, 5f);
+                }
+                //clone = Instantiate(shotObj[count], position[count], rotation[count]);    // 武器のクローンを生成
+                //shotObj[count].SetActive(false);        // 武器本体を非表示に
+                //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                //Vector3 dir = ray.direction;            // 飛ばす向き
+                //clone.GetComponent<Rigidbody>().AddForce(dir * 3000);   // マウスの位置に飛ばす
+                //count++;
 
-                // 5秒後にクローンを削除
+                //// 5秒後にクローンを削除
                 Destroy(clone, 5f);
             }
         }
