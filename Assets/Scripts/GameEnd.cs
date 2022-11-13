6ce338382b362a7eef6f620235df45da956da633
YamaokaShinsaku <20010017ys@edu.fca.ac.jp>
2022-11-13 13:51:44 +0900

    ゲーム終了処理を作成

---
+++ Assets/Scripts/GameEnd.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnd : MonoBehaviour
+{
+    void Update()
+    {
+        EndGame();
+    }
+
+    //ゲーム終了
+    private void EndGame()
+    {
+        //Escが押された時
+        if (Input.GetKey(KeyCode.Escape))
+        {
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+    Application.Quit();//ゲームプレイ終了
+#endif
+        }
+
+    }
+}
