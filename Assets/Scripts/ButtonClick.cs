ee204ab921958456dd382df064390ca4ddd541eb
YamaokaShinsaku <20010017ys@edu.fca.ac.jp>
2022-11-14 19:41:58 +0900

    noPushをどこかで呼ばないといけない（ButtonClick）

--- Assets/Scripts/ButtonClick.cs
+++ Assets/Scripts/ButtonClick.cs
@@ -9,21 +9,17 @@ public class ButtonClick : MonoBehaviour
     public void Push()
     {
         sw = true;
-        Time.timeScale = 1.0f;
     }
 
     public void noPush()
     {
         sw = false;
-        Time.timeScale = 1.0f;
     }
 
     public void Update()
     {
-        Time.timeScale = 1.0f;
         if (sw)
         {
-            Time.timeScale = 1.0f;
             Debug.Log("Push");
         }
     }
